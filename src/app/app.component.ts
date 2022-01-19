fa5c46ded9917e6f332f86f287513fedb478b553
Anuroop <anuroopjoy@gmail.com>
2022-01-19 21:37:11 +0530

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,31 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'worker-basics';
-  worker!: SharedWorker;
+  worker!: Worker;
+  sharedWorker!: SharedWorker;
 
   ngOnInit(): void {
-    if (typeof Worker !== 'undefined') {
-      // Create a new
-      console.log(import.meta.url);
-      this.worker = new SharedWorker(new URL('./app.shared.worker', import.meta.url));
-      this.worker.port.onmessage = ({ data }) => {
-        console.log(data);
-      };
-      this.worker.port.onmessageerror = (error) => {
-        console.error('Error message received from worker:', error);
-      };
-      // setTimeout(() => {
-      //   worker.terminate();
-      //   console.log('terminated');
-      // }, 1000);
-    } else {
-      // Web Workers are not supported in this environment.
-      // You should add a fallback so that your program still executes correctly.
-    }
+    console.log(import.meta.url);
+    this.#initWorker();
+    this.#initSharedWorker();
     // const result = this.#generateFibonacci(42);
     // console.log(result);
   }
 
+  #initWorker() {
+    if (typeof Worker !== 'undefined') {
+      this.worker = new Worker(new URL('./app.worker', import.meta.url));
+      this.worker.onmessage = ({ data }) => {
+        console.log('Data received from worker ', data);
+      };
+      this.worker.onerror = (error) => {
+        console.error('Error message received from worker:', error);
+      };
+    } else {
+      // Web Workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+    }
+  }
+
+  #initSharedWorker() {
+    if (typeof SharedWorker !== 'undefined') {
+      this.sharedWorker = new SharedWorker(
+        new URL('./app.shared.worker', import.meta.url)
+      );
+      this.sharedWorker.port.onmessage = ({ data }) => {
+        console.log('Data received from shared worker ', data);
+      };
+      this.sharedWorker.port.onmessageerror = (error) => {
+        console.error('Error message received from shared worker:', error);
+      };
+    } else {
+      // Shared Workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+    }
+  }
   // #generateFibonacci = (n: number): number => {
   //   if (n < 2) {
   //     return n;
@@ -40,6 +57,15 @@ export class AppComponent implements OnInit {
   // };
 
   postMsg() {
-    this.worker.port.postMessage({ action: 'generateFibonacci', param: 42 });
+    this.worker.postMessage({ action: 'generateFibonacci', param: 42 });
+  }
+  postSharedMsg() {
+    this.sharedWorker.port.postMessage({ action: 'generateFibonacci', param: 42 });
+  }
+  terminateWorker() {
+    this.worker.terminate();
+  }
+  terminateSharedWorker() {
+    this.sharedWorker.port.postMessage({ action: 'terminate' });
   }
 }
--- src/app/app.shared.worker.ts
+++ src/app/app.shared.worker.ts
@@ -12,14 +12,18 @@ const connections: MessagePort[] = [];
 self.onconnect = (connectEvent: MessageEvent) => {
   const port = connectEvent.ports[0];
   connections.push(port);
-  port.start();
+  // port.start();
 
   port.onmessage = (messageEvent) => {
     console.log('worker got message: ', messageEvent);
     // throw new Error('Test error from worker');
-    const response = generateFibonacci(messageEvent.data.param);
-    connections.forEach((connection) => {
-      connection.postMessage(response);
-    });
+    if (messageEvent.data.action === 'generateFibonacci') {
+      const response = generateFibonacci(messageEvent.data.param);
+      connections.forEach((connection) => {
+        connection.postMessage(response);
+      });
+    } else if (messageEvent.data.action === 'terminate') {
+      self.close();
+    }
   };
 };
